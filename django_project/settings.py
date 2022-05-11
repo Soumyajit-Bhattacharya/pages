6fa650fcdb649b9d56f1de69795250bc409fa40c
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-05-11 13:28:04 +0530

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^5w=id#c$k2-9%nf3a$r=_5gyqv536gjo8$^fi36iiw)6q6yqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
