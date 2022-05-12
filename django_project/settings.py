11f1bd0d05277ad98210dfa16924f0ac08cdf7ef
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-05-12 10:02:19 +0530

    hi

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^5w=id#c$k2-9%nf3a$r=_5gyqv536gjo8$^fi36iiw)6q6yqg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
